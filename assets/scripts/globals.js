d12ea667b0e529c84db772c843a0a1fbff4e2cf7
Ceo-Sammarco <noah.kd@gmail.com>
2024-04-27 14:25:06 -0300

    Ceo-Sammarco - sáb 27 abr 2024

---
+++ assets/scripts/globals.js
@@ -0,0 +1,25 @@
+
+
+"use strict";
+
+const 
+   _ = ( ...v ) => console.log( ...v )
+   ,
+   $ = v => document.querySelector( v )
+   ,
+   $$ = v => document.querySelectorAll( v )
+;
+
+addEventListener( "load", () => {
+   
+   $$( "nav-link" ).forEach( nv => {
+      nv.outerHTML = `
+         <a href="${ nv.getAttribute( "to" ) } target="_blank">
+            ${ nv.innerText }
+         </a>
+      `;
+   } );
+
+
+
+} );
